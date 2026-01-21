cc309c29dde45482a0706bdfdbc27d3b44053907
Swati Sanjay Thorat <thoratswati11@gmail.com>
2026-01-21 22:31:56 +0530

    Assignment 7

---
+++ Assignments/assignment7.ts
@@ -0,0 +1,59 @@
+function getBestDayToBuyAndSell(prices:number[]):void{
+    let bestBuyDay:number=0;
+    let bestSellDay:number=0;
+    let maxProfit:number=0;    
+    bestBuyDay=findMinimumNumPositionOfGivenNumbers(prices);
+    console.log(`Best Buy Day Position: ${bestBuyDay}`);
+    bestSellDay=findMaximumNumPositionOfGivenNumbers(prices,bestBuyDay);
+    console.log(`Best Sell Day Position: ${bestSellDay}`);
+    if(bestSellDay>bestBuyDay)
+    {
+        maxProfit=(prices[bestSellDay]??0) -(prices[bestBuyDay]??0);
+        console.log(`Best day to buy is Day ${bestBuyDay+1} at price ${prices[bestBuyDay]}`);
+        console.log(`Best day to sell is Day ${bestSellDay+1} at price ${prices[bestSellDay]}`);
+        console.log(`Maximum Profit is ${maxProfit}`);
+    }
+    else
+    {
+        console.log("case, no transactions are done and the max profit = 0.");
+    }
+}
+
+function findMinimumNumPositionOfGivenNumbers(num:number[]):number{
+
+    let min:number=num[0]??0;
+    let pos:number=0;
+    let i:number=-1;
+    for(const value of num)
+    {
+        i++;
+       if(value<min)
+       {
+           min=value;
+           pos=i;
+       }
+    }
+  return pos;
+}
+
+
+function findMaximumNumPositionOfGivenNumbers(num:number[],position:number):number{
+
+    let max:number=num[position]??0;
+    let pos:number=0;
+    let i:number=-1;
+    for(let i:number=position;i<num.length;i++)
+    {
+
+       if(num[i]!>max)
+       {
+           max=num[i]??max;
+           pos=i;
+       }
+    }
+  return pos;
+}
+
+
+getBestDayToBuyAndSell([7,1,5,3,6,4]);
+getBestDayToBuyAndSell([7,6,4,3,1]);
