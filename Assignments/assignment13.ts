7abc519753da99dcb0e7114e2ef03480391cd01b
Swati Sanjay Thorat <thoratswati11@gmail.com>
2026-01-27 09:58:29 +0530

    Assignment13

---
+++ Assignments/assignment13.ts
@@ -0,0 +1,19 @@
+function displayTriangle(rows:number):void{
+    
+    for(let i:number=0;i<=rows;i++){
+        let result:string="";
+        for(let j:number=0;j<rows;j++)
+        {   
+        if(j<rows-i) {   
+         result=result+" ";
+            }
+        else{
+        result=result+"*";
+        }
+        }
+              console.log(result) 
+    }
+
+    }
+export{};
+displayTriangle(5);
